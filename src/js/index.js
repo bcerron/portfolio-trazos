fa939b6e54d4aba02ca4680dfdd08e567ed6ed16
bcerron <pedro@bcerron.com>
2023-03-12 13:23:36 +0100

    First Version

--- src/js/index.js
+++ src/js/index.js
@@ -11,10 +11,25 @@ const screenSize = {
 
 // Obtén todos los elementos de la lista
 const menuElements = document.querySelectorAll('.menu__element');
-const blueBackgroundElement = document.querySelectorAll('.content');
+const blueBackgroundElement = document.querySelectorAll('.container');
+const footerElement = document.getElementById('footer');
+const contactButtonElement = document.getElementById('contact-button');
 
 // Función para verificar el ancho de la pantalla
 function checkWidth() {
+  //console.log(menuElements[0]);
+  if (window.innerWidth >= screenSize.desktop && contactButtonElement.classList.contains('active-element')) {
+    contactButtonElement.classList.add('hide');
+  }
+  if (window.innerWidth < screenSize.desktop) {
+    footerElement.classList.add('hide-footer');
+    contactButtonElement.classList.remove('hide-footer');
+  }
+  if (window.innerWidth >= screenSize.desktop) {
+    footerElement.classList.remove('hide-footer');
+    contactButtonElement.classList.add('hide-footer');
+  }
+
   if (window.innerWidth < screenSize.tablet) {
     // Si el ancho es menor de 700px, agrega la clase CSS "hide-span" a todos los elementos span
     menuElements.forEach(element => {
@@ -56,13 +71,14 @@ menuElements.forEach(element => {
 });
 
 const menuElement = document.getElementById('menu');
-const sections = document.querySelectorAll('.content');
+const sections = document.querySelectorAll('.article');
 
 menuElement.addEventListener('click', e => {
   console.log(e.target);
   if (!e.target.classList.contains('menu__element')) return;
 
   sections.forEach(sections => sections.classList.add('hide'));
+
   document.getElementById(e.target.dataset.menu).classList.remove('hide');
 });
 
